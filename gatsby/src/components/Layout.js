e24dccf41233387e2685ea7dcc3ab86188e55111
William Clark Newell <clark@clarknewell.tech>
2021-06-25 14:40:57 -0600

    Updating global styles, adding typography and nav styles
    Updating global styles, adding typography and nav styles

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 import 'normalize.css';
 
 export default function Layout({ children }) {
   return (
     <div>
+      <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
