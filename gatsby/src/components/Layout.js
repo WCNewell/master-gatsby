7f8c1c29bdf4f6f2b84ed9371ab35883deffb5e4
William Clark Newell <clark@clarknewell.tech>
2021-06-25 09:38:56 -0600

    Updates to Layout, adding wrapPageElement
    Updates to Layout, adding wrapPageElement

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Nav from './Nav';
+import Footer from './Footer';
 
-export default function Layout() {
+export default function Layout({ children }) {
   return (
     <div>
       <Nav />
+      {children}
+      <Footer />
     </div>
   );
 }
--- gatsby/src/pages/orders.js
+++ gatsby/src/pages/orders.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 export default function OrdersPage() {
   return (
-    <div>
+    <>
       <p>Hey! I'm the Orders Page</p>
-    </div>
+    </>
   );
 }
