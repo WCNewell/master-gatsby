688fb333f76ffd7bf5c94d5e3aa5f890bc96aa74
William Clark Newell <clark@clarknewell.tech>
2021-06-28 14:39:26 -0600

    Some backend work
    Some backend work

--- sanity/schemas/pizza.js
+++ sanity/schemas/pizza.js
@@ -1,16 +1,35 @@
+import { MdLocalPizza as icon } from 'react-icons/md';
+
 export default {
   // Computer title
   name: 'pizza',
   // Visible title
   title: 'Pizzas',
   type: 'document',
-  icon: () => '🍕',
+  icon,
   fields: [
     {
       name: 'name',
       title: 'Pizza Name',
       type: 'string',
       description: 'Name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
     },
   ],
 };
