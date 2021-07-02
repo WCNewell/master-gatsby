727b08d43d7f707c4720e310e6dcb8b6a5ce0572
William Clark Newell <clark@clarknewell.tech>
2021-07-01 22:44:32 -0600

    Added person fields to schema
    Added person fields to schema

---
+++ sanity/schemas/person.js
@@ -0,0 +1,40 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+export default {
+  // Computer title
+  name: 'person',
+  // Visible title
+  title: 'Slicemasters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Person Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'slug',
+      desription: 'Tell us about this person',
+    },
+    {
+      name: 'image',
+      title: 'image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
