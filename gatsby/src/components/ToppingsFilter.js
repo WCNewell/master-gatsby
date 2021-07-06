d99df4751c27ab6edc1f32f0424e6b02722f959d
William Clark Newell <clark@clarknewell.tech>
2021-07-06 16:39:57 -0600

    Update ToppingsFilter.js
    Expanding ToppingsFilter functionality and adding styles

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -1,29 +1,59 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import styled from 'styled-components';
+
+const ToppingsStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    .active {
+      background: var(--yellow);
+    }
+  }
+`;
 
 function countPizzasInToppings(pizzas) {
-  return pizzas
+  // return the pizzas with counts
+  const counts = pizzas
     .map((pizza) => pizza.toppings)
     .flat()
     .reduce((acc, topping) => {
       // check if this is an existing topping
       const existingTopping = acc[topping.id];
-      
       // if it is, increment by 1
-      if(existingTopping) {
-        existingTopping.count += ;1
-      }
-      // otherwise create a new entry in our acc and set it to one
-      acc[topping.id] = {
-        id: topping.id,
-        name: topping.name,
-        count: 1
+      if (existingTopping) {
+        existingTopping.count += 1;
+      } else {
+        // otherwise create a new entry in our acc and set it to one
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
       }
       return acc;
-    }, {})
+    }, {});
+  // sort them based on their count
+  const sortedToppings = Object.values(counts).sort(
+    (a, b) => b.count - a.count
+  );
+  return sortedToppings;
 }
 
-export default function ToppingsFilter() {
+export default function ToppingsFilter({ activeTopping }) {
   // Get a list of the toppings
   // Get a list of all the pizzas and their topings
   const { toppings, pizzas } = useStaticQuery(graphql`
@@ -45,19 +75,26 @@ export default function ToppingsFilter() {
       }
     }
   `);
-
-  console.clear();
-  console.log(toppings, pizzas);
   // Count how many pizzas are in each topping
   const toppingsWithCounts = countPizzasInToppings(pizzas.nodes);
-  console.log(toppingsWithCounts);
-
   // Loop over the list of toppings and display the topping and the count of pizzas in that toping
   // Link it up ...
-
   return (
-    <div>
-      <p>Toppings</p>
-    </div>
+    <ToppingsStyles>
+      <Link to="/pizzas">
+        <span className="name">All</span>
+        <span className="count">{pizzas.nodes.length}</span>
+      </Link>
+      {toppingsWithCounts.map((topping) => (
+        <Link
+          to={`/topping/${topping.name}`}
+          key={topping.id}
+          className={topping.name === activeTopping ? 'active' : ''}
+        >
+          <span className="name">{topping.name}</span>
+          <span className="count">{topping.count}</span>
+        </Link>
+      ))}
+    </ToppingsStyles>
   );
 }
