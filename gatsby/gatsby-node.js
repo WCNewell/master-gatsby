7ed3f5a95fce493af6658b5b191e92b89a4cd36c
William Clark Newell <clark@clarknewell.tech>
2021-07-07 17:31:57 -0600

    Update gatsby-node.js
    Updates to gatsby-node

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,4 +1,5 @@
 import path from 'path';
+import fetch from 'isomorphic-fetch';
 
 async function turnPizzasIntoPages({ graphql, actions }) {
   // 1. Get a template for this page
@@ -58,6 +59,40 @@ async function turnToppingsIntoPages({ graphql, actions }) {
     });
   });
   // 4. Pass topping data to pizza.js
+}
+
+async function fetchBeersandTurnIntoNodes({
+  actions,
+  createNodeId,
+  createContentDigest,
+}) {
+  // 1. Fetch list of beers
+  const res = await fetch('https://api.sampleapis.com/beers/ale');
+  const beers = await res.json();
+  console.log(beers);
+  // 2. Loop over each one
+  for (const beer of beers) {
+    const nodeMeta = {
+      id: createNodeId(`beer-${beer.name}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: 'Beer',
+        mediaType: 'application/json',
+        contentDigest: createContentDigest(beer),
+      },
+    };
+    actions.createNode({
+      ...beer,
+      ...nodeMeta,
+    });
+  }
+  // 3. Create a node for that beer
+}
+
+export async function sourceNodes(params) {
+  // Fetch a list of beers and source them into our gatsby api
+  await Promise.all([fetchBeersandTurnIntoNodes(params)]);
 }
 
 export async function createPages(params) {
