0c9b2d4626e572902f4b9e6fa03e319eefeba00a
William Clark Newell <clark@clarknewell.tech>
2021-07-13 14:16:15 -0600

    Update slicemasters.js
    Added stylesl to slicemasters.js, description is not populating with every individual, still just "current" person at this time

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,9 +1,74 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function SliceMastersPage() {
+const SlicemasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const SlicemasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: -2rem;
+    position: relative;
+    z-index: 2;
+  }
+`;
+
+export default function SliceMastersPage({ data }) {
+  const slicemasters = data.slicemasters.nodes;
+  console.log(slicemasters);
   return (
     <>
-      <p>Hey! I'm the Slice Masters Page</p>
+      <SlicemasterGrid>
+        {slicemasters.map((person) => (
+          <SlicemasterStyles>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className="description">{person.description.current}</p>
+          </SlicemasterStyles>
+        ))}
+      </SlicemasterGrid>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description {
+          current
+        }
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
