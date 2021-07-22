78153a77ca5ed771456fa4657f6b34665a21e9b4
William Clark Newell <clark@clarknewell.tech>
2021-07-22 14:50:21 -0600

    Creating custom sidebar for Sanity backend UI
    Creating custom sidebar for Sanity backend UI

--- sanity/schemas/storeSettings.js
+++ sanity/schemas/storeSettings.js
@@ -2,66 +2,29 @@ import { MdStore as icon } from 'react-icons/md';
 
 export default {
   // Computer title
-  name: 'pizza',
+  name: 'storeSettings',
   // Visible title
-  title: 'Pizzas',
+  title: 'Settings',
   type: 'document',
   icon,
   fields: [
     {
       name: 'name',
-      title: 'Pizza Name',
+      title: 'Store Name',
       type: 'string',
-      description: 'Name of the pizza',
+      description: 'Name of the store',
     },
     {
-      name: 'slug',
-      title: 'slug',
-      type: 'slug',
-      options: {
-        source: 'name',
-        maxLength: 100,
-      },
-    },
-    {
-      name: 'image',
-      title: 'image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: 'price',
-      title: 'Price',
-      type: 'number',
-      description: 'Price of the pizza in cents',
-      validation: (Rule) => Rule.min(1000),
-      inputComponent: PriceInput,
-    },
-    {
-      name: 'toppings',
-      title: 'Toppings',
+      name: 'slicemasters',
+      title: 'Slicemasters Currently Slicing',
       type: 'array',
-      of: [{ type: 'reference', to: [{ type: 'topping' }] }],
+      of: [{ type: 'reference', to: [{ type: 'person' }] }],
+    },
+    {
+      name: 'hotSlices',
+      title: 'Hot Slices available in the case',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'pizza' }] }],
     },
   ],
-  preview: {
-    select: {
-      title: 'name',
-      media: 'image',
-      topping0: 'toppings.0.name',
-      topping1: 'toppings.1.name',
-      topping2: 'toppings.2.name',
-      topping3: 'toppings.3.name',
-    },
-    prepare: ({ title, media, ...toppings }) => {
-      const tops = Object.values(toppings).filter(Boolean);
-      return {
-        title,
-        media,
-        subtitle: tops.join(', '),
-      };
-    },
-  },
 };
