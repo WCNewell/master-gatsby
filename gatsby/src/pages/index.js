b6bb0e4cfdb93680080e96cc38031737559907ad
William Clark Newell <clark@clarknewell.tech>
2021-07-25 18:40:31 -0600

    More work on Sanity customizations and front end home page
    More work on Sanity customizations and front end
    home page

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { HomePageGrid } from '../styles/Grids';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing() {
+  return (
+    <div>
+      <p>Currently Slicing</p>
+    </div>
+  );
+}
+
+function HotSlices() {
+  return (
+    <div>
+      <p>HotSlices</p>
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { slicemasters, hotSlices } = useLatestData();
   return (
-    <>
-      <p>Hey! I'm the Home Page</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza in GatsbyLand!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <HomePageGrid>
+        <CurrentlySlicing slicemasters={slicemasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </HomePageGrid>
+    </div>
   );
 }
